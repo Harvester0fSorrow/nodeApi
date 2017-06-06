50b7f4598cf6db6d55337c84449847d17c2c5404
Radoslaw Lisowski <Rad@Radoslaws-MacBook-Pro.local>
2017-06-06 22:28:48 +0100

    removed old calls

--- api/routes/apiRoutes.js
+++ api/routes/apiRoutes.js
@@ -1,12 +1,6 @@
 module.exports = function(app) {
   var controller = require('../controllers/controller');
 
-  app.route('/api/Home')
-    .get(controller.home);
-
-  app.route('/api/Metadata/menu')
-      .get(controller.metadata);
-
   app.route('/api/About')
       .get(controller.about);
 };
