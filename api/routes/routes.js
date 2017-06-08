aa3df99cad6b730de7f112d3c6073ee483f92be2
Radoslaw Lisowski <Rad@Radoslaws-MacBook-Pro.local>
2017-06-08 19:10:25 +0100

    updated html files

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,6 +1,11 @@
 var path = require('path');
 
 module.exports = function(app) {
+
+  app.get('/api/*', function(req, res){
+     res.send({error: 'route not found'});
+  });
+
   app.get('/*', function(req, res){
      res.sendFile(path.join(__dirname, '../../html', 'index.html'));
   });
