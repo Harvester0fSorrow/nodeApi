43e57ccb3dbb2fedf26bb0df9b90d76d258e1dc3
Radoslaw Lisowski <Rad@Radoslaws-MacBook-Pro.local>
2017-07-04 22:48:35 +0100

    added new route

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -2,9 +2,25 @@ var path = require('path');
 
 module.exports = function(app) {
 
+  // api routes
+
+  app.get('/api/blog', function(req, res){
+     res.send({
+       posts: [
+         {
+           title: 'First post',
+           desc: 'so much information',
+           content: 'wow',
+         },
+       ],
+     });
+  });
+
   app.get('/api/*', function(req, res){
      res.send({error: 'route not found'});
   });
+
+  // app routes
 
   app.get('/*', function(req, res){
      res.sendFile(path.join(__dirname, '../../html', 'index.html'));
