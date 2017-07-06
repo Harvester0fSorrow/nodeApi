f9f32faeb980e75fed3d3a2e0a246f5e4f9c6878
Radoslaw Lisowski <Rad@Radoslaws-MacBook-Pro.local>
2017-07-06 22:05:01 +0100

    updated routes with new blog request

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -4,15 +4,22 @@ module.exports = function(app) {
 
   // api routes
 
-  app.get('/api/blog', function(req, res){
+  app.get('/api/blog/last', function(req, res){
      res.send({
-       posts: [
+       post:
          {
-           title: 'First post',
-           desc: 'so much information',
-           content: 'wow',
+           title: 'And so it begins ...',
+           date: new Date().toDateString(),
+           content: '<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, '+
+           'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. '+
+           'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi '+
+           'ut aliquip ex ea commodo consequat. </p>'+
+           '<p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum '+
+           'dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, '+
+           'sunt in culpa qui officia deserunt mollit anim id est laborum.</p>'+
+           '<p>Next time I should write something in english...</p>'+
+           '<p style="text-indent:0;text-align: center;margin-top:50px;margin-bottom:0;">to be continued ...</span>',
          },
-       ],
      });
   });
 
